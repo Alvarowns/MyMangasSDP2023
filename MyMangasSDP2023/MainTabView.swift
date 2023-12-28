8621b8414f6a72c947d73cc26d5dfdcd14c3e41b
Alvarowns <alvs.iosdev@gmail.com>
2023-12-28 08:49:21 +0100

    CoverView en proceso

---
+++ MyMangasSDP2023/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  MyMangasSDP2023
+//
+//  Created by Alvaro Santos Orellana on 28/12/23.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
