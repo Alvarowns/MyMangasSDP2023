fd5c70abb51ef66ab954b34e8f5061bb9e7b7ddb
Alvarowns <alvs.iosdev@gmail.com>
2023-12-28 19:27:32 +0100

    DemogTypeView added

---
+++ MyMangasSDP2023/DemogTypeView.swift
@@ -0,0 +1,18 @@
+//
+//  DemogTypeView.swift
+//  MyMangasSDP2023
+//
+//  Created by Alvaro Santos Orellana on 28/12/23.
+//
+
+import SwiftUI
+
+struct DemogTypeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DemogTypeView()
+}
