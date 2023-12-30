3a6c9b54db764e344de97bdad03d539cfac840e6
Alvarowns <alvs.iosdev@gmail.com>
2023-12-30 11:42:28 +0100

    Creating MyCollectionView with SwiftData

---
+++ MyMangasSDP2023/BackButton.swift
@@ -0,0 +1,18 @@
+//
+//  BackButton.swift
+//  MyMangasSDP2023
+//
+//  Created by Alvaro Santos Orellana on 29/12/23.
+//
+
+import SwiftUI
+
+struct BackButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackButton()
+}
---
+++ MyMangasSDP2023/MyCollectionView.swift
@@ -0,0 +1,18 @@
+//
+//  MyCollectionView.swift
+//  MyMangasSDP2023
+//
+//  Created by Alvaro Santos Orellana on 30/12/23.
+//
+
+import SwiftUI
+
+struct MyCollectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyCollectionView()
+}
