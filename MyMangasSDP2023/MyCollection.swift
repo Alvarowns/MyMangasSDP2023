90bd3f3775e0df18798165cf9445b9b45aafb0e0
Alvarowns <alvs.iosdev@gmail.com>
2023-12-31 02:42:10 +0100

    MyCollectionView editing

---
+++ MyMangasSDP2023/MyCollection.swift
@@ -0,0 +1,18 @@
+//
+//  MyCollection.swift
+//  MyMangasSDP2023
+//
+//  Created by Alvaro Santos Orellana on 30/12/23.
+//
+
+import SwiftUI
+
+struct MyCollection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyCollection()
+}
