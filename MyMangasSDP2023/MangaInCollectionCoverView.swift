14845fc557c533b1ae24fcd5757002ff8f545db0
Alvarowns <alvs.iosdev@gmail.com>
2024-01-03 16:39:37 +0100

    Preparando paginación en ExploreView

---
+++ MyMangasSDP2023/MangaInCollectionCoverView.swift
@@ -0,0 +1,18 @@
+//
+//  MangaInCollectionCoverView.swift
+//  MyMangasSDP2023
+//
+//  Created by Alvaro Santos Orellana on 1/1/24.
+//
+
+import SwiftUI
+
+struct MangaInCollectionCoverView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MangaInCollectionCoverView()
+}
