a6aa0fa2ba3289c7ee697509ee5038e83fa14b3f
Alvarowns <alvs.iosdev@gmail.com>
2024-01-03 23:10:58 +0100

    Actualizada la función getMangas(), ahora agrega mangas de páginas siguientes a otro array. Actualizada ExploreView() con dicho array, al hacer scroll va cargando más mangas. La alerta que salta al agregar un manga a la colección ahora hace dismiss del sheet que lo genera al pulsar "OK".

---
+++ MyMangasSDP2023/RandomMangaView.swift
@@ -0,0 +1,18 @@
+//
+//  RandomMangaView.swift
+//  MyMangasSDP2023
+//
+//  Created by Alvaro Santos Orellana on 3/1/24.
+//
+
+import SwiftUI
+
+struct RandomMangaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RandomMangaView()
+}
