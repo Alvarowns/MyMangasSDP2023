9aaa508c0f36af727c86c9d3747150b5cb092e72
Alvarowns <alvs.iosdev@gmail.com>
2024-01-15 08:37:21 +0100

    Building profileView.

---
+++ MyMangasSDP2023/ProfileModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileModel.swift
+//  MyMangasSDP2023
+//
+//  Created by Alvaro Santos Orellana on 15/1/24.
+//
+
+import Foundation
---
+++ MyMangasSDP2023/ProfileVM.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileVM.swift
+//  MyMangasSDP2023
+//
+//  Created by Alvaro Santos Orellana on 15/1/24.
+//
+
+import Foundation
