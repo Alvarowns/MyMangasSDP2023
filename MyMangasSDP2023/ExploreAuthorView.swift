cdc5549155820773b1484af540859b67d435da64
Alvarowns <alvs.iosdev@gmail.com>
2024-01-23 08:30:17 +0100

    Realizado: Añadido filtro de búsqueda por temas. Añadido filtro de búsqueda por autores (crashea por ser muchísimos, actualmente deshabilitado). bestMangas se actualiza y rellena al llegar al último del scroll.

---
+++ MyMangasSDP2023/ExploreAuthorView.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreAuthorView.swift
+//  MyMangasSDP2023
+//
+//  Created by Alvaro Santos Orellana on 23/1/24.
+//
+
+import SwiftUI
+
+struct ExploreAuthorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExploreAuthorView()
+}
---
+++ MyMangasSDP2023/ExploreThemeView.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreThemeView.swift
+//  MyMangasSDP2023
+//
+//  Created by Alvaro Santos Orellana on 23/1/24.
+//
+
+import SwiftUI
+
+struct ExploreThemeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExploreThemeView()
+}
